6d24464542070f5c718ae4bd98bf49af8746d5db
Ev Liu <azn_logik@yahoo.com>
2023-11-30 21:15:47 -0800

    stylelize list

--- lib/src/weight/weight_item_list_view.dart
+++ lib/src/weight/weight_item_list_view.dart
@@ -36,36 +36,47 @@ class WeightItemListView extends StatelessWidget {
       // In contrast to the default ListView constructor, which requires
       // building all Widgets up front, the ListView.builder constructor lazily
       // builds Widgets as they’re scrolled into view.
-      body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
-        restorationId: 'weightItemListView',
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          final item = items[index];
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.blue, Colors.blueGrey],
+            ),
+          ),
+          child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListView.builder(
+                // Providing a restorationId allows the ListView to restore the
+                // scroll position when a user leaves and returns to the app after it
+                // has been killed while running in the background.
+                restorationId: 'weightItemListView',
+                itemCount: items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = items[index];
 
-          return Card(
-              child: ListTile(
-                  title: Text('${item.weight.toString()} lbs',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      )),
-                  subtitle:
-                      Text(item.dateTime, style: const TextStyle(fontSize: 10)),
-                  onTap: () {
-                    // Navigate to the details page. If the user leaves and returns to
-                    // the app after it has been killed while running in the
-                    // background, the navigation stack is restored.
-                    Navigator.restorablePushNamed(
-                      context,
-                      WeightItemDetailsView.routeName,
-                    );
-                  }));
-        },
-      ),
+                  return Card(
+                    child: ListTile(
+                        title: Text('${item.weight.toString()} lbs',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            )),
+                        subtitle: Text(item.dateTime,
+                            style: const TextStyle(fontSize: 10)),
+                        onTap: () {
+                          // Navigate to the details page. If the user leaves and returns to
+                          // the app after it has been killed while running in the
+                          // background, the navigation stack is restored.
+                          Navigator.restorablePushNamed(
+                            context,
+                            WeightItemDetailsView.routeName,
+                          );
+                        }),
+                  );
+                },
+              ))),
     );
   }
 }
